d50f9f627bb83029e1268899b99e57e5447afcbf
Widua <mikolaj.widla@gmail.com>
2025-07-23 11:54:43 +0200

    Implemented echo

--- src/main/java/handlers/ClientHandler.java
+++ src/main/java/handlers/ClientHandler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class ClientHandler implements Runnable{
 
@@ -22,8 +23,20 @@ public class ClientHandler implements Runnable{
                 byte[] inputData = new byte[100];
 
                 input.read(inputData);
-                System.out.println("Received: "+new String(inputData));
-                output.write("+PONG\r\n".getBytes());
+                String[] command = new String(inputData).split("\r\n");
+                if (command.length == 1){
+                    break;
+                }
+                switch (command[2].toLowerCase()){
+                    case "ping" -> {
+                        output.write("+PONG\r\n".getBytes());
+                    }
+                    case "echo" -> {
+                        String arg = command[4];
+                        output.write(String.format("$%s\r\n%s\r\n",arg.length(),arg).getBytes());
+                    }
+                }
+
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
