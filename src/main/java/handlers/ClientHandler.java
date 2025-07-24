f41615d246c12b7cf8f678a82536ced58c40b0e9
Widua <mikolaj.widla@gmail.com>
2025-07-24 17:22:02 +0200

    Implemented SET, and GET commands

--- src/main/java/handlers/ClientHandler.java
+++ src/main/java/handlers/ClientHandler.java
@@ -5,10 +5,13 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClientHandler implements Runnable{
 
     private final Socket clientSocket;
+    private Map<String,String> vars = new HashMap<>();
 
     public ClientHandler(Socket clientSocket){
         this.clientSocket = clientSocket;
@@ -34,6 +37,20 @@ public class ClientHandler implements Runnable{
                     case "echo" -> {
                         String arg = command[4];
                         output.write(String.format("$%s\r\n%s\r\n",arg.length(),arg).getBytes());
+                    }
+                    case "set" -> {
+                        String argName = command[4];
+                        String argVal = command[6];
+                        vars.put(argName,argVal);
+                        output.write("+OK\r\n".getBytes());
+                    }
+                    case "get" -> {
+                        String argName = command[4];
+                        if (!vars.containsKey(argName)){
+                            output.write("$-1\r\n".getBytes());
+                        }
+                        String argVal = vars.get(argName);
+                        output.write(String.format("$%s\r\n%s\r\n",argVal.length(),argVal).getBytes());
                     }
                 }
 
