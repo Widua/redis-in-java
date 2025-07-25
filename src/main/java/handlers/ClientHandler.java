7fc757cab44efb7e3a7c261d908595fd13f5dec4
Widua <mikolaj.widla@gmail.com>
2025-07-25 17:07:59 +0200

    Implemented px to set command

--- src/main/java/handlers/ClientHandler.java
+++ src/main/java/handlers/ClientHandler.java
@@ -4,53 +4,66 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable {
 
     private final Socket clientSocket;
-    private Map<String,String> vars = new HashMap<>();
+    private Map<String, String> vars = new HashMap<>();
 
-    public ClientHandler(Socket clientSocket){
+    public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
     }
 
     @Override
     public void run() {
-        while (true){
-            try{
+        while (true) {
+            try {
                 InputStream input = clientSocket.getInputStream();
                 OutputStream output = clientSocket.getOutputStream();
                 byte[] inputData = new byte[100];
 
                 input.read(inputData);
                 String[] command = new String(inputData).split("\r\n");
-                if (command.length == 1){
+                if (command.length == 1) {
                     break;
                 }
-                switch (command[2].toLowerCase()){
+                switch (command[2].toLowerCase()) {
                     case "ping" -> {
                         output.write("+PONG\r\n".getBytes());
                     }
                     case "echo" -> {
                         String arg = command[4];
-                        output.write(String.format("$%s\r\n%s\r\n",arg.length(),arg).getBytes());
+                        output.write(String.format("$%s\r\n%s\r\n", arg.length(), arg).getBytes());
                     }
                     case "set" -> {
                         String argName = command[4];
                         String argVal = command[6];
-                        vars.put(argName,argVal);
+                        vars.put(argName, argVal);
                         output.write("+OK\r\n".getBytes());
+
+                        if (command.length > 8){
+                            String ex = command[8];
+                            new Thread( () -> {
+                                if (ex.equalsIgnoreCase("px")){
+                                    try {
+                                        Thread.sleep(Long.parseLong(command[10]));
+                                        vars.remove(argName);
+                                    } catch (InterruptedException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                }
+                            } ).start();
+                        }
                     }
                     case "get" -> {
                         String argName = command[4];
-                        if (!vars.containsKey(argName)){
+                        if (!vars.containsKey(argName)) {
                             output.write("$-1\r\n".getBytes());
                         }
                         String argVal = vars.get(argName);
-                        output.write(String.format("$%s\r\n%s\r\n",argVal.length(),argVal).getBytes());
+                        output.write(String.format("$%s\r\n%s\r\n", argVal.length(), argVal).getBytes());
                     }
                 }
 
